1c2adcbce914ed4ccc2c8d2240abc5d4f8c82c1f
Naomi1027 <guerrillawish@hotmail.com>
2025-02-28 15:14:56 +0900

    MassageテーブルとSeederの作成

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Message;
 
 class MessageSeeder extends Seeder
 {
@@ -12,6 +13,27 @@ class MessageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //メッセージデータの作成
+        Message::create([
+            'thread_id' => 1,
+            'message_en' => 'Hello, how are you?',
+            'message_ja' => 'こんにちは、元気ですか？',
+            'sender' => '1', //ユーザー
+            'audio_file_path' => 'audio/1.mp3',
+        ]);
+        Message::create([
+            'thread_id' => 1,
+            'message_en' => 'I am fine, thank you.',
+            'message_ja' => '元気です、ありがとう。',
+            'sender' => '2', //AI
+            'audio_file_path' => 'audio/2.mp3',
+        ]);
+        Message::create([
+            'thread_id' => 1,
+            'message_en' => 'What is your name?',
+            'message_ja' => 'あなたの名前は何ですか？',
+            'sender' => '1', //ユーザー
+            'audio_file_path' => 'audio/3.mp3',
+        ]);
     }
 }
