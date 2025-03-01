9b994c7c55fb03eb677a23384c0cffef1455e631
Naomi1027 <guerrillawish@hotmail.com>
2025-03-01 18:11:52 +0900

    gptにリクエストを送信してレスポンスをdbに保存

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -45,6 +45,20 @@ class MessageController extends Controller
                 return response()->json(['message' => 'API呼び出しに失敗しました'], 500);
             }
 
+            // 過去のメッセージを取得
+            $messages = Message::where('thread_id', $threadId)->get();
+            // GPTにAPIリクエスト
+            $gptResponse = $apiService->callGptApi($messages);
+            $aiMessageText = $gptResponse['choices'][0]['message']['content'];
+            // データベースに保存する処理を追加
+            $aiMessage = Message::create([
+                'thread_id' => $threadId,
+                'message_en' => $aiMessageText,
+                'message_ja' => '',
+                'audio_file_path' => '',
+                'sender' => 2, // AI
+            ]);
+
         return response()->json(['message' => '音声データが保存されました'], 200);
     }
     return response()->json(['message' => '音声データが保存されませんでした'], 400);
