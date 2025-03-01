604a7072699b94d0220e6cdf0b63a60ab378e5cc
Naomi1027 <guerrillawish@hotmail.com>
2025-03-01 16:57:12 +0900

    音声データをAPIに送信

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Message;
+use App\Http\Services\ApiService;
 
 class MessageController extends Controller
 {
@@ -16,7 +17,7 @@ class MessageController extends Controller
             $timestamp = now()->format('YmdHis');
             $path = $audio->storeAs('audio', "audio_{$timestamp}.wav", 'public'); // 音声データを保存
 
-            // 必要に応じて、データベースに保存する処理を追加
+            // データベースに保存する処理
             $message = Message::create([
                 'thread_id' => $threadId,
                 'message_en' => 'dummy',
@@ -24,6 +25,17 @@ class MessageController extends Controller
                 'audio_file_path' => $path,
                 'sender' => 1, // ユーザー
             ]);
+
+            // 音声データをAPIに送信
+            $apiService = new ApiService();
+            $response = $apiService->callWhiperApi($path);
+            $message_en = $response['text'];
+
+            $message->update([
+                'message_en' => $message_en,
+            ]);
+
+
         return response()->json(['message' => '音声データが保存されました'], 200);
     }
     return response()->json(['message' => '音声データが保存されませんでした'], 400);
