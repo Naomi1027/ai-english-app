07c12ef22f05d309b062ffd50bce242a2ef2690e
Naomi1027 <guerrillawish@hotmail.com>
2025-03-03 17:10:05 +0900

    vercel

--- database/seeders/ThreadSeeder.php
+++ database/seeders/ThreadSeeder.php
@@ -13,41 +13,41 @@ class ThreadSeeder extends Seeder
     public function run(): void
     {
         //スレッドデータの作成
-        Thread::create([
-            'title' => '英語学習',
-        ]);
-        Thread::create([
-            'title' => '英会話の練習',
-        ]);
-        Thread::create([
-            'title' => '英会話スレッド１',
-        ]);
-        Thread::create([
-            'title' => '英会話スレッド２',
-        ]);
-        Thread::create([
-            'title' => '英会話スレッド３',
-        ]);
-        Thread::create([
-            'title' => '英会話スレッド４',
-        ]);
-        Thread::create([
-            'title' => '英会話スレッド５',
-        ]);
-        Thread::create([
-            'title' => '英会話スレッド６',
-        ]);
-        Thread::create([
-            'title' => '英会話スレッド７',
-        ]);
-        Thread::create([
-            'title' => '英会話スレッド８',
-        ]);
-        Thread::create([
-            'title' => '英会話スレッド９',
-        ]);
-        Thread::create([
-            'title' => '英会話スレッド１０',
-        ]);
+        // Thread::create([
+        //     'title' => '英語学習',
+        // ]);
+        // Thread::create([
+        //     'title' => '英会話の練習',
+        // ]);
+        // Thread::create([
+        //     'title' => '英会話スレッド１',
+        // ]);
+        // Thread::create([
+        //     'title' => '英会話スレッド２',
+        // ]);
+        // Thread::create([
+        //     'title' => '英会話スレッド３',
+        // ]);
+        // Thread::create([
+        //     'title' => '英会話スレッド４',
+        // ]);
+        // Thread::create([
+        //     'title' => '英会話スレッド５',
+        // ]);
+        // Thread::create([
+        //     'title' => '英会話スレッド６',
+        // ]);
+        // Thread::create([
+        //     'title' => '英会話スレッド７',
+        // ]);
+        // Thread::create([
+        //     'title' => '英会話スレッド８',
+        // ]);
+        // Thread::create([
+        //     'title' => '英会話スレッド９',
+        // ]);
+        // Thread::create([
+        //     'title' => '英会話スレッド１０',
+        // ]);
     }
 }
